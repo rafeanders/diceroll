0594bc74d1fd3592385446e21a53cf503f47788a
wankerpants <wankerpants@gmail.com>
2021-11-04 00:10:13 -0500

    Finished adding all the dice

--- app/src/main/java/com/wankercraft/dice/RollActivity.java
+++ app/src/main/java/com/wankercraft/dice/RollActivity.java
@@ -70,18 +70,18 @@ public class RollActivity extends Activity {
                         isRolling = false;
                         break;
                     }
-                    /*case 10: {
-                        v.setBackground(ImageObjects.D10Image(rollValue));
+                    case 10: {
+                        v.setBackground(D10Image(rollValue));
                         break;
                     }
                     case 12: {
-                        v.setBackground(ImageObjects.D12Image(rollValue));
+                        v.setBackground(D12Image(rollValue));
                         break;
                     }
                     case 20: {
-                        v.setBackground(ImageObjects.D20Image(rollValue));
+                        v.setBackground(D20Image(rollValue));
                         break;
-                    }*/
+                    }
                 }
             }
         });
@@ -137,13 +137,13 @@ public class RollActivity extends Activity {
         Drawable d4BG = getDrawable(R.drawable.d4_4);
         Drawable d6BG = getDrawable(R.drawable.d6_6);
         Drawable d8BG = getDrawable(R.drawable.d8_8);
-        //Drawable d10BG = io.getDrawable(R.drawable.d10_10);
-        //Drawable d12BG = io.getDrawable(R.drawable.d12_12);
-        //Drawable d20BG = io.getDrawable(R.drawable.d20_20);
+        Drawable d10BG = getDrawable(R.drawable.d10_10);
+        Drawable d12BG = getDrawable(R.drawable.d12_12);
+        Drawable d20BG = getDrawable(R.drawable.d20_20);
 
         // Logic to return correct image based which number sided die the user selected.
         switch (i) {
-            /* case 20: {
+            case 20: {
                 return d20BG;
             }
             case 12: {
@@ -151,7 +151,7 @@ public class RollActivity extends Activity {
             }
             case 10: {
                 return d10BG;
-            }*/
+            }
             case 8: {
                 return d8BG;
             }
@@ -255,240 +255,198 @@ public class RollActivity extends Activity {
             }
         } return null;
     }
-    /*
+
     //D10 images
-    public static Drawable D10Image(int i) {
-        Drawable d10One = io.getDrawable(R.drawable.d10_1);
-        Drawable d10Two = io.getDrawable(R.drawable.d10_2);
-        Drawable d10Three = io.getDrawable(R.drawable.d10_3);
-        Drawable d10Four = io.getDrawable(R.drawable.d10_4);
-        Drawable d10Five = io.getDrawable(R.drawable.d10_5);
-        Drawable d10Six = io.getDrawable(R.drawable.d10_6);
-        Drawable d10Seven = io.getDrawable(R.drawable.d10_7);
-        Drawable d10Eight = io.getDrawable(R.drawable.d10_8);
-        Drawable d10Nine = io.getDrawable(R.drawable.d10_9);
-        Drawable d10Ten = io.getDrawable(R.drawable.d10_10);
+    public Drawable D10Image(int i) {
+        Drawable d10One = getDrawable(R.drawable.d10_1);
+        Drawable d10Two = getDrawable(R.drawable.d10_2);
+        Drawable d10Three = getDrawable(R.drawable.d10_3);
+        Drawable d10Four = getDrawable(R.drawable.d10_4);
+        Drawable d10Five = getDrawable(R.drawable.d10_5);
+        Drawable d10Six = getDrawable(R.drawable.d10_6);
+        Drawable d10Seven = getDrawable(R.drawable.d10_7);
+        Drawable d10Eight = getDrawable(R.drawable.d10_8);
+        Drawable d10Nine = getDrawable(R.drawable.d10_9);
+        Drawable d10Ten = getDrawable(R.drawable.d10_10);
 
         switch (i) {
             case 10: {
                 return d10Ten;
-                break;
             }
             case 9: {
                 return d10Nine;
-                break;
             }
             case 8: {
                 return d10Eight;
-                break;
             }
             case 7: {
                 return d10Seven;
-                break;
             }
             case 6: {
                 return d10Six;
-                break;
             }
             case 5: {
                 return d10Five;
-                break;
             }
             case 4: {
                 return d10Four;
-                break;
             }
             case 3: {
                 return d10Three;
-                break;
             }
             case 2: {
                 return d10Two;
-                break;
             }
             case 1: {
                 return d10One;
-                break;
             }
         } return null;
     }
 
     //D12 images
-    public static Drawable D12Image(int i) {
-        Drawable d12One = io.getDrawable(R.drawable.d12_1);
-        Drawable d12Two = io.getDrawable(R.drawable.d12_2);
-        Drawable d12Three = io.getDrawable(R.drawable.d12_3);
-        Drawable d12Four = io.getDrawable(R.drawable.d12_4);
-        Drawable d12Five = io.getDrawable(R.drawable.d12_5);
-        Drawable d12Six = io.getDrawable(R.drawable.d12_6);
-        Drawable d12Seven = io.getDrawable(R.drawable.d12_7);
-        Drawable d12Eight = io.getDrawable(R.drawable.d12_8);
-        Drawable d12Nine = io.getDrawable(R.drawable.d12_9);
-        Drawable d12Ten = io.getDrawable(R.drawable.d12_10);
-        Drawable d12Eleven = io.getDrawable(R.drawable.d12_11);
-        Drawable d12Twelve = io.getDrawable(R.drawable.d12_12);
+    public Drawable D12Image(int i) {
+        Drawable d12One = getDrawable(R.drawable.d12_1);
+        Drawable d12Two = getDrawable(R.drawable.d12_2);
+        Drawable d12Three = getDrawable(R.drawable.d12_3);
+        Drawable d12Four = getDrawable(R.drawable.d12_4);
+        Drawable d12Five = getDrawable(R.drawable.d12_5);
+        Drawable d12Six = getDrawable(R.drawable.d12_6);
+        Drawable d12Seven = getDrawable(R.drawable.d12_7);
+        Drawable d12Eight = getDrawable(R.drawable.d12_8);
+        Drawable d12Nine = getDrawable(R.drawable.d12_9);
+        Drawable d12Ten = getDrawable(R.drawable.d12_10);
+        Drawable d12Eleven = getDrawable(R.drawable.d12_11);
+        Drawable d12Twelve = getDrawable(R.drawable.d12_12);
 
         switch (i) {
             case 12: {
                 return d12Twelve;
-                break;
             }
             case 11: {
                 return d12Eleven;
-                break;
             }
             case 10: {
                 return d12Ten;
-                break;
             }
             case 9: {
                 return d12Nine;
-                break;
             }
             case 8: {
                 return d12Eight;
-                break;
             }
             case 7: {
                 return d12Seven;
-                break;
             }
             case 6: {
                 return d12Six;
-                break;
             }
             case 5: {
                 return d12Five;
-                break;
             }
             case 4: {
                 return d12Four;
-                break;
             }
             case 3: {
                 return d12Three;
-                break;
             }
             case 2: {
                 return d12Two;
-                break;
             }
             case 1: {
                 return d12One;
-                break;
             }
         } return null;
     }
 
     //D20 images
-    public static Drawable D20Image(int i) {
-        Drawable d20One = io.getDrawable(R.drawable.d20_1);
-        Drawable d20Two = io.getDrawable(R.drawable.d20_2);
-        Drawable d20Three = io.getDrawable(R.drawable.d20_3);
-        Drawable d20Four = io.getDrawable(R.drawable.d20_4);
-        Drawable d20Five = io.getDrawable(R.drawable.d20_5);
-        Drawable d20Six = io.getDrawable(R.drawable.d20_6);
-        Drawable d20Seven = io.getDrawable(R.drawable.d20_7);
-        Drawable d20Eight = io.getDrawable(R.drawable.d20_8);
-        Drawable d20Nine = io.getDrawable(R.drawable.d20_9);
-        Drawable d20Ten = io.getDrawable(R.drawable.d20_10);
-        Drawable d20Eleven = io.getDrawable(R.drawable.d20_11);
-        Drawable d20Twelve = io.getDrawable(R.drawable.d20_12);
-        Drawable d20Thirteen = io.getDrawable(R.drawable.d20_13);
-        Drawable d20Fourteen = io.getDrawable(R.drawable.d20_14);
-        Drawable d20Fifteen = io.getDrawable(R.drawable.d20_15);
-        Drawable d20Sixteen = io.getDrawable(R.drawable.d20_16);
-        Drawable d20Seventeen = io.getDrawable(R.drawable.d20_17);
-        Drawable d20Eighteen = io.getDrawable(R.drawable.d20_18);
-        Drawable d20Nineteen = io.getDrawable(R.drawable.d20_19);
-        Drawable d20Twenty = io.getDrawable(R.drawable.d20_20);
+    public Drawable D20Image(int i) {
+        Drawable d20One = getDrawable(R.drawable.d20_1);
+        Drawable d20Two = getDrawable(R.drawable.d20_2);
+        Drawable d20Three = getDrawable(R.drawable.d20_3);
+        Drawable d20Four = getDrawable(R.drawable.d20_4);
+        Drawable d20Five = getDrawable(R.drawable.d20_5);
+        Drawable d20Six = getDrawable(R.drawable.d20_6);
+        Drawable d20Seven = getDrawable(R.drawable.d20_7);
+        Drawable d20Eight = getDrawable(R.drawable.d20_8);
+        Drawable d20Nine = getDrawable(R.drawable.d20_9);
+        Drawable d20Ten = getDrawable(R.drawable.d20_10);
+        Drawable d20Eleven = getDrawable(R.drawable.d20_11);
+        Drawable d20Twelve = getDrawable(R.drawable.d20_12);
+        Drawable d20Thirteen = getDrawable(R.drawable.d20_13);
+        Drawable d20Fourteen = getDrawable(R.drawable.d20_14);
+        Drawable d20Fifteen = getDrawable(R.drawable.d20_15);
+        Drawable d20Sixteen = getDrawable(R.drawable.d20_16);
+        Drawable d20Seventeen = getDrawable(R.drawable.d20_17);
+        Drawable d20Eighteen = getDrawable(R.drawable.d20_18);
+        Drawable d20Nineteen = getDrawable(R.drawable.d20_19);
+        Drawable d20Twenty = getDrawable(R.drawable.d20_20);
 
         switch (i) {
             case 20: {
                 return d20Twenty;
-                break;
             }
             case 19: {
                 return d20Nineteen;
-                break;
             }
             case 18: {
                 return d20Eighteen;
-                break;
             }
             case 17: {
                 return d20Seventeen;
-                break;
             }
             case 16: {
                 return d20Sixteen;
-                break;
             }
             case 15: {
                 return d20Fifteen;
-                break;
             }
             case 14: {
                 return d20Fourteen;
-                break;
             }
             case 13: {
                 return d20Thirteen;
-                break;
             }
             case 12: {
                 return d20Twelve;
-                break;
             }
             case 11: {
                 return d20Eleven;
-                break;
             }
             case 10: {
                 return d20Ten;
-                break;
             }
             case 9: {
                 return d20Nine;
-                break;
             }
             case 8: {
                 return d20Eight;
-                break;
             }
             case 7: {
                 return d20Seven;
-                break;
             }
             case 6: {
                 return d20Six;
-                break;
             }
             case 5: {
                 return d20Five;
-                break;
             }
             case 4: {
                 return d20Four;
-                break;
             }
             case 3: {
                 return d20Three;
-                break;
             }
             case 2: {
                 return d20Two;
-                break;
             }
             case 1: {
                 return d20One;
-                break;
             }
         } return null;
     }
 
 
-     */
+
     //d100 images yeeeesh
 
 }
