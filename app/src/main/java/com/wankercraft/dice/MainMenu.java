9dde46281e4a7c3f1e4c50611d00124c00324d42
wankerpants <wankerpants@gmail.com>
2021-12-03 14:18:49 -0600

    finishing main menu button logic
    commenting code
    
    still some uix tweaks to be done

--- app/src/main/java/com/wankercraft/dice/MainMenu.java
+++ app/src/main/java/com/wankercraft/dice/MainMenu.java
@@ -1,12 +1,15 @@
 package com.wankercraft.dice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.wankercraft.dice.databinding.ActivityMainMenuBinding;
 
-public class MainMenu extends Activity {
+public class MainMenu extends Activity implements View.OnClickListener {
 
     private TextView mTextView;
     private ActivityMainMenuBinding binding;
@@ -19,5 +22,36 @@ public class MainMenu extends Activity {
         setContentView(binding.getRoot());
 
         mTextView = binding.mainmenuHead;
+
+        // Instantiate Buttons with onlcick listener
+        Button mButtonSelectDie = findViewById(R.id.tosidesbutton);
+        mButtonSelectDie.setOnClickListener(this);
+        Button mButtonSelectColour = findViewById(R.id.todiestylebutton);
+        mButtonSelectColour.setOnClickListener(this);
+        Button mButtonFlipCoin = findViewById(R.id.tocoinflipbutton);
+        mButtonFlipCoin.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // Start main die roll activity
+            case R.id.tosidesbutton: {
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            }
+
+            // Start colour/style selection activity
+            //case R.id.todiestylebutton: {
+            //    startActivity(new Intent(this, SelectColour.class));
+            //    break;
+
+            // Start Coin Flip activity
+            //case R.id.tocoinflipbutton: {
+            //    startActivity(new Intent(this, CoinFlip.class));
+            //    break;
+        }
     }
 }
