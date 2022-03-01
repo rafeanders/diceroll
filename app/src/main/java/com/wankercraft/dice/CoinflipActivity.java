7bf5910f35c38fbb3b3f20d7e44d0cb817dd2024
wankerpants <wankerpants@gmail.com>
2022-02-28 22:14:15 -0600

    coin flip logic work

--- app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
+++ app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
@@ -18,8 +18,12 @@ public class CoinflipActivity extends Activity {
 
     private boolean isFlipping; // is the coin being flipped
 
+    private float degreesRotation = 720f; // how many degrees of rotation
+    private int animationDuration = 1000; // how long it should take
+
     private Drawable coinHeads;
     private Drawable coinTails;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,28 +43,22 @@ public class CoinflipActivity extends Activity {
             if (!isFlipping) {
                 isFlipping = true;
 
-                mFlipButton.animate(). {
-
-                }
                 //start flipping animation i may miss understand right now what the 720 is
-                mFlipButton.animate().rotationX(mFlipButton.getRotation() - 720).start();
-                // while the animation sill has duration i think. i think thats wqhat th 720 is
+                mFlipButton.animate().setDuration(animationDuration).rotationXBy(degreesRotation).start();
+                // while the animation sill has duration change image based on position
                 while(mFlipButton.animate().getDuration() != 0) {
-                    if((mFlipButton.getRotationX() >= 0 && mFlipButton.getRotationX() <= 180) || (mFlipButton.getRotationX() > 360 && mFlipButton.getRotationX() <= 540)) {
+                    if ((mFlipButton.getRotationX() >= 0 && mFlipButton.getRotationX() <= 90) || (mFlipButton.getRotationX() > 270 && mFlipButton.getRotationX() <= 360)) {
                         mFlipButton.setBackground(coinHeads);
-                    } else if((mFlipButton.getRotationX() > 180 && mFlipButton.getRotationX() <= 360) || (mFlipButton.getRotationX() > 540 && mFlipButton.getRotationX() <= 720)) {
+                    } else if (mFlipButton.getRotationX() > 90 && mFlipButton.getRotationX() <= 270) {
                         mFlipButton.setBackground(coinTails);
                     }
                 }
-                mFlipButton.animate().rota
             }
         });
     }
 
-    trying method for this.
-    private void TossAnimation() {
-        mFlipButton.animate().setDuration(1000).rotationXBy(720f);
-    }
+    // coin is heads face up at 0 degrees and 360 degrees, and tails face up at 180 degrees
+
 
     private int CoinToss() {
         Random r = new Random();
