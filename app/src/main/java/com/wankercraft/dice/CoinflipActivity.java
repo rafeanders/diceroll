02a17e6f15b0db718ec3e2e2db0b7da8ce0ac755
wankerpants <wankerpants@gmail.com>
2022-03-05 13:56:05 -0600

    more flip animation head smashing

--- app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
+++ app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
@@ -4,15 +4,22 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.net.ipsec.ike.TunnelModeChildSessionParams;
 import android.os.Bundle;
+import android.os.Debug;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.wankercraft.dice.databinding.ActivityCoinflipBinding;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 //TODO: fix flipping animation
-
 public class CoinflipActivity extends Activity {
 
     private ImageButton mFlipButton;
@@ -20,39 +27,17 @@ public class CoinflipActivity extends Activity {
     private ActivityCoinflipBinding binding;
 
     private boolean isFlipping; // is the coin being flipped
-    private volatile boolean isRunning = false; // bool for thread
+    private boolean animRunning; //is animation running
+    private boolean isHeads;
 
     private float degreesRotation = 720f; // how many degrees of rotation default will land on heads
     private int animationDuration = 1000; // how long it should take
 
+    private String flipResult;
+
     private Drawable coinHeads;
     private Drawable coinTails;
-
-    Runnable animRunnable = new Runnable() {
-        @Override
-        public void run() {
-            while(mFlipButton.animate().getDuration() != 0) {
-                if ((mFlipButton.getRotationX() >= 0 && mFlipButton.getRotationX() <= 90) || (mFlipButton.getRotationX() > 270 && mFlipButton.getRotationX() <= 360)) {
-                    mFlipButton.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mFlipButton.setBackground(coinHeads);
-                        }
-                    });
-                } else if (mFlipButton.getRotationX() > 90 && mFlipButton.getRotationX() <= 270) {
-                    mFlipButton.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mFlipButton.setBackground(coinTails);
-                        }
-                    });
-                }
-            }
-            if (!isRunning) return;
-            //isFlipping = false;
-        }
-    };
-
+    private Drawable coinSide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,29 +53,31 @@ public class CoinflipActivity extends Activity {
         mFlipButton = findViewById(R.id.coiflipButton);
         mFlipButton.setBackground(coinHeads);
 
-        Thread animThread = new Thread(animRunnable);
-
         mFlipButton.setOnClickListener(v -> {
             if (!isFlipping) {
                 isFlipping = true;
                 //do the coin flip
                 CoinToss();
                 //start flipping animation
-                mFlipButton.animate().setDuration(animationDuration).rotationXBy(degreesRotation).start();
-                // while the animation sill has duration change image based on position
-                animThread.start(); // do the while loop that's changine the image?
+                mFlipButton.animate().setDuration(animationDuration).rotationXBy(degreesRotation).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        mFlipButton.setBackground(coinSide);
+                    }
+                }).start();
             }
-
             isFlipping = false;
         });
     }
 
     private void CoinToss() {
         Random r = new Random();
-        // 0 == heads 1 == tails
-        float tossValue = r.nextFloat();
+        isHeads = r.nextBoolean(); // randomly set isHeads to true or false
+        coinSide = isHeads ? coinHeads : coinTails;
         // set flip rotation total amount based on heads or tails so coin lands on heads or tails. 0/360/720 degrees is heads, 180/540/900 is tails
-        degreesRotation = tossValue > 0.5f ? 900f : 720f;
+        //degreesRotation = isHeads ? 900f : 720f; // set the final rotation of coin based on heads or tails
+        flipResult = isHeads ? "Heads" : "Tails"; // set the result string based on flip
+        Log.d("CoinTossResult", flipResult);
     }
 
 
