ac943fbf7e705b82c0a7b9a3e94e9f8f9165284e
wankerpants <wankerpants@gmail.com>
2022-03-05 10:21:57 -0600

    working on coin toss animation stuff

--- app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
+++ app/src/main/java/com/wankercraft/dice/CoinflipActivity.java
@@ -2,6 +2,7 @@ package com.wankercraft.dice;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.net.ipsec.ike.TunnelModeChildSessionParams;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import com.wankercraft.dice.databinding.ActivityCoinflipBinding;
 
 import java.util.Random;
 
+//TODO: fix flipping animation
+
 public class CoinflipActivity extends Activity {
 
     private ImageButton mFlipButton;
@@ -17,12 +20,38 @@ public class CoinflipActivity extends Activity {
     private ActivityCoinflipBinding binding;
 
     private boolean isFlipping; // is the coin being flipped
+    private volatile boolean isRunning = false; // bool for thread
 
     private float degreesRotation = 720f; // how many degrees of rotation default will land on heads
     private int animationDuration = 1000; // how long it should take
 
     private Drawable coinHeads;
     private Drawable coinTails;
+
+    Runnable animRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while(mFlipButton.animate().getDuration() != 0) {
+                if ((mFlipButton.getRotationX() >= 0 && mFlipButton.getRotationX() <= 90) || (mFlipButton.getRotationX() > 270 && mFlipButton.getRotationX() <= 360)) {
+                    mFlipButton.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mFlipButton.setBackground(coinHeads);
+                        }
+                    });
+                } else if (mFlipButton.getRotationX() > 90 && mFlipButton.getRotationX() <= 270) {
+                    mFlipButton.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mFlipButton.setBackground(coinTails);
+                        }
+                    });
+                }
+            }
+            if (!isRunning) return;
+            //isFlipping = false;
+        }
+    };
 
 
     @Override
@@ -39,25 +68,19 @@ public class CoinflipActivity extends Activity {
         mFlipButton = findViewById(R.id.coiflipButton);
         mFlipButton.setBackground(coinHeads);
 
+        Thread animThread = new Thread(animRunnable);
+
         mFlipButton.setOnClickListener(v -> {
             if (!isFlipping) {
                 isFlipping = true;
-
                 //do the coin flip
                 CoinToss();
-
-                //start flipping animation i may miss understand right now what the 720 is
+                //start flipping animation
                 mFlipButton.animate().setDuration(animationDuration).rotationXBy(degreesRotation).start();
                 // while the animation sill has duration change image based on position
-                // curently this bit locks up haard
-                //while(mFlipButton.animate().getDuration() != 0) {
-                //    if ((mFlipButton.getRotationX() >= 0 && mFlipButton.getRotationX() <= 90) || (mFlipButton.getRotationX() > 270 && mFlipButton.getRotationX() <= 360)) {
-                //        mFlipButton.setBackground(coinHeads);
-                //    } else if (mFlipButton.getRotationX() > 90 && mFlipButton.getRotationX() <= 270) {
-                //        mFlipButton.setBackground(coinTails);
-                //    }
-                //}
+                animThread.start(); // do the while loop that's changine the image?
             }
+
             isFlipping = false;
         });
     }
@@ -69,4 +92,7 @@ public class CoinflipActivity extends Activity {
         // set flip rotation total amount based on heads or tails so coin lands on heads or tails. 0/360/720 degrees is heads, 180/540/900 is tails
         degreesRotation = tossValue > 0.5f ? 900f : 720f;
     }
+
+
+
 }
